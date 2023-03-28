842536cb0a57ee98a5be96a4f2f0c361fe1d5544
username <ayspina@gmail.com>
2023-03-28 16:25:15 -0400

    Updates About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,9 @@
 export default function About() {
     return(
         <>
-        <h1>About Me</h1>
+        <h1>Ayana Spina</h1>
+        <h2>Full-Stack Sofware Engineer</h2>
+        <img src="https://i.imgur.com/WGLgrhR.png" alt="GoobyDoo and I" />
         </>
     )
 }
